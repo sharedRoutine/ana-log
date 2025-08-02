982d47c25d6258c96c3d9a9591db707606cd5465
Janosch Hübner <dev@jnsh.me>
2025-08-02 16:39:25 +0200

    add basic dark mode support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,19 +6,31 @@ import deMessages from '../locales/de.json';
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 import { db } from '~/db/db';
 import migrations from '../drizzle/migrations';
+import { useColorScheme } from 'nativewind';
 
 export default function Layout() {
   const { success, error } = useMigrations(db, migrations);
 
+  const { colorScheme } = useColorScheme();
+
   return (
     <IntlProvider locale="de" messages={deMessages}>
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: true }} />
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: true,
+            headerTitleStyle: { color: colorScheme === 'light' ? '#000' : '#fff' },
+            headerStyle: { backgroundColor: colorScheme === 'light' ? undefined : 'black' },
+          }}
+        />
         <Stack.Screen
           name="add-item"
           options={{
             presentation: 'modal',
             headerShown: true,
+            headerTitleStyle: { color: colorScheme === 'light' ? '#000' : '#fff' },
+            headerStyle: { backgroundColor: colorScheme === 'light' ? undefined : 'black' },
           }}
         />
         <Stack.Screen
@@ -26,6 +38,8 @@ export default function Layout() {
           options={{
             presentation: 'modal',
             headerShown: true,
+            headerTitleStyle: { color: colorScheme === 'light' ? '#000' : '#fff' },
+            headerStyle: { backgroundColor: colorScheme === 'light' ? undefined : 'black' },
           }}
         />
         <Stack.Screen
@@ -33,6 +47,8 @@ export default function Layout() {
           options={{
             presentation: 'modal',
             headerShown: true,
+            headerTitleStyle: { color: colorScheme === 'light' ? '#000' : '#fff' },
+            headerStyle: { backgroundColor: colorScheme === 'light' ? undefined : 'black' },
           }}
         />
       </Stack>
--- app/index.tsx
+++ app/index.tsx
@@ -5,32 +5,39 @@ import { useIntl } from 'react-intl';
 import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
 import { db } from '~/db/db';
 import { itemTable } from '~/db/schema';
+import { useColorScheme } from 'nativewind';
 
 export default function Home() {
   const router = useRouter();
   const intl = useIntl();
 
+  const { colorScheme } = useColorScheme();
+
   const { data } = useLiveQuery(db.select().from(itemTable));
 
   return (
-    <>
+    <View className="flex-1 bg-white dark:bg-black">
       <Stack.Screen
         options={{
           title: intl.formatMessage({ id: 'app.title' }),
           headerRight: () => (
             <View className="flex-row">
               <TouchableOpacity onPress={() => router.push('/settings')} className="mr-3">
-                <Ionicons name="settings-outline" size={24} color="#000" />
+                <Ionicons
+                  name="settings-outline"
+                  size={24}
+                  color={colorScheme === 'light' ? '#000' : '#fff'}
+                />
               </TouchableOpacity>
               <TouchableOpacity onPress={() => router.push('/add-item')} className="mr-2">
-                <Ionicons name="add" size={24} color="#000" />
+                <Ionicons name="add" size={24} color={colorScheme === 'light' ? '#000' : '#fff'} />
               </TouchableOpacity>
             </View>
           ),
         }}
       />
-      <View className="px-4 pt-4">
-        <Text className="mb-4 text-lg font-bold">
+      <View className="bg-white px-4 pt-4 dark:bg-black">
+        <Text className="mb-4 text-lg font-bold text-white dark:text-gray-300">
           {data.length === 1
             ? intl.formatMessage({ id: 'home.number-of-items-created.single' })
             : intl.formatMessage({ id: 'home.number-of-items-created' }, { count: data.length })}
@@ -46,6 +53,6 @@ export default function Home() {
           </View>
         </TouchableOpacity>
       </View>
-    </>
+    </View>
   );
 }
