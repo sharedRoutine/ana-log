207f7cb3acf2a23953121a92db1fa325343af98f
Janosch Hübner <dev@jnsh.me>
2025-08-02 16:15:32 +0200

    add dark mode by class name

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
